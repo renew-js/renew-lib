4f704541ae40a55d2efe32d8044a6417787b6645
Tim Kilian <mail@timkilian.pl>
2019-01-14 15:48:45 +0100

    Minify dist bundle, rename to renew-lib.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,22 @@ import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 
 export default {
-    input: 'src/main.js',
+    input: 'src/index.js',
     output: {
-        file: 'dist/bundle.js',
+        file: 'dist/renew-lib.js',
+        name: 'renew-lib',
         format: 'umd',
     },
     plugins: [
         resolve({ customResolveOptions: { moduleDirectory: 'node_modules' } }),
         babel({ exclude: 'node_modules/**' }),
         json(),
-        commonjs()
+        commonjs(),
+        uglify()
     ],
     external: [],
 };
