73b512b6ef966ed3d6645ee403189b3d3c0a8aea
Tim Kilian <mail@timkilian.pl>
2019-01-28 20:27:09 +0100

    Style connection

--- src/draw/MetaRenderer.js
+++ src/draw/MetaRenderer.js
@@ -9,24 +9,41 @@ import { stringify } from 'svgson';
 export default class MetaRenderer extends BaseRenderer {
     /**
      * @param {Object} eventBus
+     * @param {Styles} styles
      */
-    constructor (eventBus) {
+    constructor (eventBus, styles) {
         super(eventBus, 10);
+        this.styles = styles;
     }
 
     canRender (element) {
-        return element.body;
+        return element.body || element.waypoints;
     }
 
     drawShape (graphics, element) {
-        const circle = create('svg');
-        innerSVG(circle, stringify(element.body));
-        append(graphics, circle);
-        return circle;
+        const shape = create('svg');
+        innerSVG(shape, stringify(element.body));
+        append(graphics, shape);
+        return shape;
     }
 
     drawConnection (graphics, connection) {
-        console.log(connection);
+        const line = create('polyline');
+
+        attr(line, {
+            points: connection.waypoints.reduce((result, point) => {
+                return result + point.x + ',' + point.y + ' ';
+            }, ''),
+        });
+        attr(line, this.styles.style([ 'no-fill' ], {
+            stroke: 'black',
+            strokeWidth: 2,
+            fill: 'none'
+        }));
+
+        append(graphics, line);
+
+        return line;
     }
 
     getShapePath (shape) {
