1ae6a4850e754954520151b5bf2815df933dc32d
Tim Kilian <mail@timkilian.pl>
2019-02-03 15:01:37 +0100

    Add resize to place and transition

--- src/features/meta-modeling/MetaResize.js
+++ src/features/meta-modeling/MetaResize.js
@@ -11,26 +11,35 @@ export class MetaResize {
     init () {
         this.eventBus.on('resize.start', this.onResizeStart.bind(this));
         this.eventBus.on('resize.move', this.onResizeMove.bind(this));
-        this.eventBus.on('resize.end', this.onResizeEnd.bind(this));
+        this.eventBus.on('resize.end', 1501, this.onResizeEnd.bind(this));
     }
 
     onResizeStart (event) {
     }
 
     onResizeMove (event) {
-        this.resizeElement(event.shape.body);
-    }
-
-    /**
-     * Recursive resize of elements and children
-     * @param {Object} element svgson object
-     */
-    resizeElement (element) {
-        element.children.forEach((child) => {
-            this.resizeElement(child);
-        });
     }
 
     onResizeEnd (event) {
+        let bounds = event.context.newBounds;
+        let proportions = event.shape.body.proportions;
+        switch (event.shape.body.name) {
+            case 'ellipse':
+                event.shape.body.attributes.cx = proportions.cx * bounds.width;
+                event.shape.body.attributes.cy = proportions.cy * bounds.height;
+                event.shape.body.attributes.rx = proportions.rx * bounds.width;
+                event.shape.body.attributes.ry = proportions.ry * bounds.height;
+                break;
+            case 'circle':
+                event.shape.body.attributes.cx = proportions.cx * bounds.width;
+                event.shape.body.attributes.cy = proportions.cy * bounds.height;
+                event.shape.body.attributes.r = proportions.r * Math.min(bounds.width, bounds.height);
+                break;
+            case 'rect':
+                event.shape.body.attributes.x = proportions.x * bounds.width;
+                event.shape.body.attributes.y = proportions.y * bounds.height;
+                event.shape.body.attributes.width = proportions.width * bounds.width;
+                event.shape.body.attributes.height = proportions.height * bounds.height;
+        }
     }
 }
