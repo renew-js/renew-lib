a4445658cf59aa3bc76946728885a8fd4d62cd88
Tim Kilian <mail@timkilian.pl>
2019-02-27 09:57:07 +0100

    Add context-tools to metaContextPad

--- src/features/meta-formalism/MetaContextPad.js
+++ src/features/meta-formalism/MetaContextPad.js
@@ -1,33 +1,73 @@
 export class MetaContextPad {
-    constructor (contextPad, eventBus, modeling) {
-        this.metaContextPadEntries = {};
-        
+    constructor (contextPad, metaPluginManager, modeling) {
         this.contextPad = contextPad;
-        this.eventBus = eventBus;
+        this.pluginManager = metaPluginManager;
         this.modeling = modeling;
 
-        this.eventBus.on('plugin.registered', (event) => {
-            this.registerPlugin(event.plugin);
-            this.contextPad.registerProvider(this);
-        });
+        this.contextPad.registerProvider(this);
     }
 
     getContextPadEntries (element) {
-        return {
+        const entries = {
             'delete': {
                 group: 'edit',
                 className: 'context-pad-icon-remove',
                 title: 'Remove',
                 action: {
-                    click: () => this.modeling.removeElements([element])
+                    click: () => this.modeling.removeElements([ element ]),
                 },
             },
         };
+
+        const plugin = this.pluginManager.getPlugin(element.model);
+        const metaModel = plugin.getMetaModel();
+
+        metaModel.relations.forEach((relation) => {
+            const relationEntry = this.getRelationEntry(element, relation);
+            if (relationEntry) {
+                entries[ relation.type ] = relationEntry;
+            }
+        });
+
+        metaModel.getElement(element.metaType).labels.forEach((label) => {
+            const textEntry = this.getTextEntry(element, label);
+            if (textEntry) {
+                entries[ label ] = textEntry;
+            }
+        });
+
+        return entries;
     }
 
-    registerPlugin (plugin) {
+    getRelationEntry (element, relation) {
+        const config = this.pluginManager.getToolConfiguration(element.model);
+        const mapping = config.contextToolMappings[relation.type];
+
+        if (!mapping || !relation.bind[element.metaType]) {
+            return false;
+        }
+
+        return {
+            group: element.model,
+            imageUrl: mapping.icon,
+            title: mapping.title,
+        }
+    }
+
+    getTextEntry (element, label) {
+        const plugin = this.pluginManager.getPlugin(element.model);
         const toolConfiguration = plugin.getToolConfiguration();
+        const mapping = toolConfiguration.contextToolMappings[label];
 
 
+        if (!mapping) {
+            return false;
+        }
+
+        return {
+            group: element.model,
+            imageUrl: mapping.icon,
+            title: mapping.title,
+        }
     }
 }
