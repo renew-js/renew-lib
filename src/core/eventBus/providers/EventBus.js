609d32d3cfca910fbbbc7985363eca1767306c91
Tim Kilian <mail@timkilian.pl>
2019-03-11 15:52:16 +0100

    Add syntax to directly call 3rd crumb as function in behaviors

--- src/core/eventBus/providers/EventBus.js
+++ src/core/eventBus/providers/EventBus.js
@@ -15,9 +15,17 @@ export default class EventBus extends DiagramJsEventBus {
     }
 
     registerBehavior (name, priority, behavior) {
-        this.on(name + '.start', priority, behavior.before.bind(behavior));
-        this.on(name, priority, behavior.during.bind(behavior));
-        this.on(name + '.end', priority, behavior.after.bind(behavior));
+        const breadcrumbs = name.split('.');
+
+        if (breadcrumbs.length > 2) {
+            if (typeof behavior[breadcrumbs[2]] === 'function') {
+                this.on(name, behavior[breadcrumbs[2]].bind(behavior));
+            }
+        } else {
+            this.on(name + '.start', priority, behavior.before.bind(behavior));
+            this.on(name, priority, behavior.during.bind(behavior));
+            this.on(name + '.end', priority, behavior.after.bind(behavior));
+        }
     }
 
 }
