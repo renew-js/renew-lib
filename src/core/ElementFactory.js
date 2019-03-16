1a7f2e6d3dbd45fe1cff64ba4f36293d6ca7b45e
5kilian <5kilian@informatik.uni-hamburg.de>
2019-03-16 22:46:52 +0100

    Add type to every element

---
+++ src/core/ElementFactory.js
@@ -0,0 +1,13 @@
+import BaseElementFactory from 'diagram-js/lib/core/ElementFactory';
+
+
+export class ElementFactory extends BaseElementFactory {
+
+    constructor () {
+        super();
+    }
+
+    create (type, attrs) {
+        return super.create(type, Object.assign(attrs, { type: type }));
+    }
+}
