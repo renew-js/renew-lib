69c5546ef7530a0bb110b93dfdf4ecfa05124cc1
5kilian <5kilian@informatik.uni-hamburg.de>
2019-03-17 01:42:32 +0100

    Add empty hand tool

---
+++ src/features/hand/behaviors/MoveCanvasBehavior.js
@@ -0,0 +1,10 @@
+import { Behavior } from '../../../core/eventBus/Behavior';
+
+
+export class MoveCanvasBehavior extends Behavior {
+
+    constructor () {
+        super();
+    }
+
+}
---
+++ src/features/hand/tools/HandTool.js
@@ -0,0 +1,24 @@
+import { Tool } from '../../../core/toolbox/Tool';
+
+
+export class HandTool extends Tool {
+
+    constructor () {
+        super();
+    }
+
+    onDisable (event) {
+    }
+
+    onEnable (event) {
+    }
+
+    onMouseDown (event) {
+    }
+
+    onMouseMove (event) {
+    }
+
+    onMouseUp (event) {
+    }
+}
