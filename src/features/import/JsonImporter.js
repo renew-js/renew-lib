037e45ae948b9a45fba8a0cdbf13b6333e20b632
Marc Richter <marc@richter.bz>
2019-03-25 18:53:42 +0100

    Use ElementFactory to create elements

--- src/features/import/JsonImporter.js
+++ src/features/import/JsonImporter.js
@@ -1,7 +1,33 @@
+import Refs from 'object-refs';
+
+
+const parentRefs = new Refs({
+    name: 'children',
+    enumerable: true,
+    collection: true,
+}, {
+    name: 'parent',
+});
+
+const outgoingRefs = new Refs({
+    name: 'outgoing',
+    collection: true,
+}, {
+    name: 'source',
+});
+
+const incomingRefs = new Refs({
+    name: 'incoming',
+    collection: true,
+}, {
+    name: 'target',
+});
+
 export default class JsonImporter {
 
-    constructor (baseImporter) {
+    constructor (baseImporter, elementFactory) {
         this.baseImporter = baseImporter;
+        this.elementFactory = elementFactory;
     }
 
     /**
@@ -10,7 +36,13 @@ export default class JsonImporter {
      * @return {object}      The parsed import data
      */
     import (json) {
-        const data = JSON.parse(json);
+        const data = JSON.parse(json) || {};
+        data.elements = data.elements || [];
+        data.elements = data.elements.map((element) => {
+            delete element.labels;
+            delete element.children;
+            return this.elementFactory.create(element.type, element);
+        });
         this.baseImporter.import(data);
         return data;
     }
