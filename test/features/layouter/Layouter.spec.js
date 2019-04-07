fe00807002f3b0a98a2f1dee806bd8c706660a03
Marc Richter <marc@richter.bz>
2019-04-07 14:46:50 +0200

    Fix: Use DefaultFactory to actually test MetaLayouter

--- test/features/layouter/Layouter.spec.js
+++ test/features/layouter/Layouter.spec.js
@@ -5,27 +5,15 @@ import { Tester } from '../../Tester';
 describe('modules/layouter - Layouter', () => {
     let diagram;
     let layouter;
-    let elementFactory;
+    let defaultFactory;
     let canvas;
-    let shape1;
-    let shape2;
-    let connection1;
 
     beforeEach(() => {
         diagram = new Tester({ modules: [ LayouterModule ] });
+
         layouter = diagram.get('layouter');
-        elementFactory = diagram.get('elementFactory');
+        defaultFactory = diagram.get('defaultFactory');
         canvas = diagram.get('canvas');
-
-        shape1 = elementFactory.createShape({
-            id: 'shape1', x: 100, y: 200, width: 300, height: 400,
-        });
-        shape2 = elementFactory.createShape({
-            id: 'shape2', x: 500, y: 200, width: 300, height: 400,
-        });
-
-        canvas.addShape(shape1);
-        canvas.addShape(shape2);
     });
 
     it('should be defined', () => {
@@ -35,16 +23,30 @@ describe('modules/layouter - Layouter', () => {
     describe('Provider', () => {
 
         it('should layout a connection', () => {
-            const connection1 = elementFactory.createConnection({
+            const shape1 = defaultFactory.createShape({
+                id: 'shape1',
+                x: 100,
+                y: 200,
+            });
+            const shape2 = defaultFactory.createShape({
+                id: 'shape2',
+                x: 200,
+                y: 300,
+            });
+
+            canvas.addShape(shape1);
+            canvas.addShape(shape2);
+
+            const connection1 = defaultFactory.createConnection({
                 source: shape1, target: shape2,
             });
 
             connection1.waypoints = layouter.layoutConnection(connection1);
 
-            expect(connection1.waypoints[0].x).toBe(250);
-            expect(connection1.waypoints[0].y).toBe(400);
-            expect(connection1.waypoints[1].x).toBe(650);
-            expect(connection1.waypoints[1].y).toBe(400);
+            expect(connection1.waypoints[0].x).toBe(139);
+            expect(connection1.waypoints[0].y).toBe(231);
+            expect(connection1.waypoints[1].x).toBe(209);
+            expect(connection1.waypoints[1].y).toBe(301);
         });
 
     });
