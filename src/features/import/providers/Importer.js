f698e89a98c162ff43ecc57c78ced372b18f2488
Marc Richter <marc@richter.bz>
2019-08-18 19:30:52 +0200

    Get connection bbox

--- src/features/import/providers/Importer.js
+++ src/features/import/providers/Importer.js
@@ -53,10 +53,9 @@ export class Importer {
 
     createLabel (element) {
         if (element.parent && element.parent.waypoints) {
-            const connectionGfx = this.canvas.getGraphics(element.parent);
-            const connectionBBox = connectionGfx.getBBox();
-            element.x += connectionBBox.x + connectionBBox.width / 2;
-            element.y += connectionBBox.y + connectionBBox.height / 2;
+            const bbox = this.canvas.getConnectionBBox(element.parent);
+            element.x += bbox.x + bbox.width / 2;
+            element.y += bbox.y + bbox.height / 2;
         }
         this.createShape(element);
         this.eventBus.fire('edit.update', { element });
